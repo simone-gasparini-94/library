54f49e747103d4bed6dba25c0750ce19873fa077
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2024-12-06 16:06:12 +0100

    manually pushed a book to check if the display function works

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+const library = [];
 
 function Book(name, author, pages, read) {
     this.name = name;
@@ -7,11 +7,31 @@ function Book(name, author, pages, read) {
     this.read = read;
 }
 
+/*
+
 function addBookToLibrary() {
     const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
+    library.push(newBook);
 }
 
-myLibrary.push(new Book("1984", "Orwell", 250, "yes"));
-myLibrary.push(new Book("1984", "Orwell", 250, "yes"));
-console.log(myLibrary);
+*/
+
+library.push(new Book("1984", "George Orwell", 328, true));
+
+function displayLibrary() {
+    const libraryContainer = document.querySelector(".library");
+    libraryContainer.innerHTML = "";
+
+    for (let i = 0; i < library.length; i++) {
+        const book = library[i];
+
+        const card = document.createElement("div");
+        card.classList.add("card");
+        
+        card.innerHTML = `${book.name}, ${book.author}, ${book.pages}, ${book.read ? "Yes" : "No"}`;
+
+        libraryContainer.appendChild(card);
+    };
+};
+
+displayLibrary();
